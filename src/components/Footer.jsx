8c63283eab60481732bcfee8c0f73039240ecd07
Arnold <vallejosarnold600@gmail.com>
2021-09-27 13:32:54 +0200

    primera parte

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+const logos = [
+  {
+    textoAternativo:"CineArtv",
+    srcDelLogo:"http://cine.ar/img/tv.cine.ar.svg",
+  },
+  {
+    textoAternativo:"CineArplay",
+    srcDelLogo:"http://cine.ar/img/play.cine.ar.svg",
+  },
+  {
+    textoAternativo:"CinArestrenos",
+    srcDelLogo:"http://cine.ar/img/estrenos.cine.ar.svg",
+  },
+];
+
+const  transformarLogo = logos.map(
+  ({ textoAlternativo, srcDelLogo }) => (
+    <li className="li-logo">
+      <a className="a-logo" href="/">
+        <img
+          className="img-logo "
+          src={srcDelLogo}
+          alt={textoAlternativo}
+        />
+      </a>
+    </li>
+  )
+);
+
+
 
 const PieDePagina = () => {
-  return <footer>pie de pagina</footer>;
+  return <footer>
+   <div className = "logoCineAr">
+    <ul></ul>
+   </div>  
+  </footer>;
+
+
 };
 
 export default PieDePagina;
