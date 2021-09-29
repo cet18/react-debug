d1a31e914f46adc8ec92e1a4d2bc9bded6799088
Arnold <vallejosarnold600@gmail.com>
2021-09-29 16:53:10 +0200

    parte 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,38 @@
 import React from "react";
+import "./styles/Footer.css"
 const logos = [
   {
-    textoAternativo:"CineArtv",
-    srcDelLogo:"http://cine.ar/img/tv.cine.ar.svg",
+    textoAternativo: "CineArtv",
+    srcDelLogo: "http://cine.ar/img/tv.cine.ar.svg",
   },
   {
-    textoAternativo:"CineArplay",
-    srcDelLogo:"http://cine.ar/img/play.cine.ar.svg",
+    textoAternativo: "CineArplay",
+    srcDelLogo: "http://cine.ar/img/play.cine.ar.svg",
   },
   {
-    textoAternativo:"CinArestrenos",
-    srcDelLogo:"http://cine.ar/img/estrenos.cine.ar.svg",
+    textoAternativo: "CinArestrenos",
+    srcDelLogo: "http://cine.ar/img/estrenos.cine.ar.svg",
   },
 ];
 
-const  transformarLogo = logos.map(
-  ({ textoAlternativo, srcDelLogo }) => (
-    <li className="li-logo">
-      <a className="a-logo" href="/">
-        <img
-          className="img-logo "
-          src={srcDelLogo}
-          alt={textoAlternativo}
-        />
-      </a>
-    </li>
-  )
-);
-
-
+const transformarLogo = logos.map(({ textoAlternativo, srcDelLogo }) => (
+  <li className="li-logo">
+    <a className="a-logo" href="/">
+      <img className="img-logo " src={srcDelLogo} alt={textoAlternativo} />
+    </a>
+  </li>
+));
 
 const PieDePagina = () => {
-  return <footer>
-   <div className = "logoCineAr">
-    <ul></ul>
-   </div>  
-  </footer>;
-
-
+  return (
+    <footer>
+      <div className="logoCineAr">
+        <ul>
+          <li>{transformarLogo}</li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default PieDePagina;
